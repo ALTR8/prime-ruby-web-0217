e5dde5c07a03a735096f793b631fa5fc2fcf2deb
Alexandra Roth <alroth8@gmail.com>
2017-01-10 01:07:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+
+  elsif n%2 == 0 || (n%3 == 0 && n != 3) ||  (n%5 == 0 && n != 5) || (n%7 == 0 && n !=7)
+     return false
+  else
+     return true
+  end
+
+end
